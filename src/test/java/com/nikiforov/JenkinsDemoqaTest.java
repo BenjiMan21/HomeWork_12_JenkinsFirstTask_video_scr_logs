8ba0ad6fd2e8471a1eb22b2d23a6f9715433eac1
Alex Nikiforov <unleash211@gmail.com>
2024-02-05 18:14:13 +0400

    add selenoid remote

--- src/test/java/com/nikiforov/JenkinsDemoqaTest.java
+++ src/test/java/com/nikiforov/JenkinsDemoqaTest.java
@@ -20,10 +20,11 @@ public class JenkinsDemoqaTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-//        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 //        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
@@ -33,7 +34,7 @@ public class JenkinsDemoqaTest {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         step("Открываем раздел automation-practice-form", ()-> {
-            open("https://demoqa.com/automation-practice-form");
+            open("/automation-practice-form");
             executeJavaScript("$('#fixedban').remove()");
             executeJavaScript("$('footer').remove()");
         });
